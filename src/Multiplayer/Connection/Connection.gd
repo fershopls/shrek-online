9b94a65c67c42b29b8add06fa47eeb98c179bb52
Fercho Posting <shaaf4ever@gmail.com>
2021-12-05 14:39:41 -0600

    Multiplayer

---
+++ src/Multiplayer/Connection/Connection.gd
@@ -0,0 +1,25 @@
+extends Node
+
+func _on_Server_button_up():
+	create_server(6969, 5)
+
+
+func _on_Client_button_up():
+	create_client('127.0.0.1', 6969)
+
+
+func create_server(port, max_players):
+	print("Server listening on port %s" % port)
+	var peer = NetworkedMultiplayerENet.new()
+	peer.set_bind_ip('0.0.0.0')
+	peer.create_server(int(port), max_players)
+	get_tree().network_peer = peer
+	Multiplayer.play()
+
+
+func create_client(server, port):
+	print("Connecting to ", server, ":", port)
+	var peer = NetworkedMultiplayerENet.new()
+	var result = peer.create_client(server, int(port))
+	get_tree().network_peer = peer
+	Multiplayer.play()
---
+++ src/Multiplayer/MultiplayerSingleton.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_on_network_peer_connected")
+	get_tree().connect("connection_failed", self, "_connection_failed")
+
+func play():
+	get_tree().change_scene("res://src/World/World.tscn")
+	yield(get_tree(), "idle_frame")
+	get_tree().current_scene.add_shrek().add_brain_local()
+
+func _connection_failed():
+	print ('Connection failed.')
+
+
+func _on_network_peer_connected(id):
+	print("Peer %s connected." % id)
+	get_tree().current_scene.add_shrek()
+
